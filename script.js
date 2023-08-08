e1dcc8108458c5f13e7b0e018c77796d294c995b
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-08-09 01:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const size = document.getElementById("sizeInfo");
+
+window.addEventListener("resize" , function(){
+	size.innerText = ""
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	let heading = document.createElement("h1");
+	heading.innerText = "Width: "+width + " Height: "+height;
+	size.append(heading);
+	
+})
